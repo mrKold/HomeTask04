b54b0d459ee7bbf0a5b0e5486d0a30ad9b1c9935
IvanK <mr.koldamasov@mail.ru>
2022-10-13 18:10:11 +0300

    выдаёт количество цифр в числе

---
+++ Ex026/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает 
+//на вход число и выдаёт количество цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while (n > 0)
+{
+    n = n / 10;
+    result = result + 1;
+}
+Console.WriteLine(result);
