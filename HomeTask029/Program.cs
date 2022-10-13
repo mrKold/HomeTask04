50c7f09d0a40714c3e9fe841fa3b9542f70c251a
IvanK <mr.koldamasov@mail.ru>
2022-10-13 20:36:38 +0300

    массив из 8 элементов задаёт ПОЛЬЗОВАТЕЛЬ

---
+++ HomeTask029/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//ЗНАЧЕНИЯ ДОЛЖЕН ВВОДИТЬ ПОЛЬЗОВАТЕЛЬ
+
+Console.Clear();
+
+Console.WriteLine("Введите первое число массива: ");
+Console.WriteLine("Введите второе число массива: ");
+Console.WriteLine("Введите третье число массива: ");
+Console.WriteLine("Введите четвертое число массива: ");
+Console.WriteLine("Введите пятое число массива: ");
+Console.WriteLine("Введите шестое число массива: ");
+Console.WriteLine("Введите седьмое число массива: ");
+Console.WriteLine("Введите восьмое число массива: ");
+
+int[] array = new int[8];
+for ( int i = 0; i < 8; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
