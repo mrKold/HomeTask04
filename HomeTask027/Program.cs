4fb15b9053e39e424761f3b42da25f8b3a8ccca9
IvanK <mr.koldamasov@mail.ru>
2022-10-13 20:10:39 +0300

    выдаёт сумму цифр в заданном числе

---
+++ HomeTask027/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int i = 0;
+
+while (n > 0)
+{
+    i = n % 10;
+    n = n / 10;
+    result = result + i;
+}
+Console.WriteLine(result);
