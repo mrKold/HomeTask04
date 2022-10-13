456553400169d1d49ec89b5e937d84dac5c5766a
IvanK <mr.koldamasov@mail.ru>
2022-10-13 19:39:20 +0300

    возводит число A в натуральную степень B

---
+++ HomeTask025/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+//возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.Clear();
+Console.WriteLine("Введите число a: ");
+Console.WriteLine("Введите число b: ");
+
+double a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+double result = 1;
+
+for (int i = 0; i < b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine(result);
