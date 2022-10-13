252331630d7eb60f6a7720281bb8ac0ce5cc2fe0
IvanK <mr.koldamasov@mail.ru>
2022-10-13 17:56:59 +0300

    выдаёт сумму чисел от 1 до n

---
+++ Ex024/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает 
+//на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+
+double n = Convert.ToInt32(Console.ReadLine());
+//int n = Convert.ToInt32(Console.ReadLine());
+// int i = 1, result = 0;
+// while (i<= n)
+// {
+//     result += i;
+//     i++;
+// }
+// Console.WriteLine(result);
+Console.WriteLine((1 + n) / 2 * n);
