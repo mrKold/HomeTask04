5f3167573d367f86b1de4d79ce31d2d485fc6e48
IvanK <mr.koldamasov@mail.ru>
2022-10-14 14:58:41 +0300

    поиск secondmax

---
+++ HomeTaskFindSecondMax/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+Console.Clear();
+
+Console.WriteLine("Введите числа: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+int secondmax = -1;
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+    if (n > max)
+    {
+        secondmax = max;
+        max = n;
+    }
+    else if (n > secondmax)
+    {
+        secondmax = n;
+    }
+}
+
+Console.WriteLine(secondmax);
